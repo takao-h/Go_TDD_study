a9153c96fd9d7ec7930b9f2d5c5d4978af1ce615
takao-h <takao.hayashi@zozo.com>
2023-07-02 23:24:23 +0900

    pass test

--- hello.go
+++ hello.go
@@ -12,19 +12,21 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
+	return greetingPrefix(language) + name
+}
 
-	prefix := englishHelloPrefix
-
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
