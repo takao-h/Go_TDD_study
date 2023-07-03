0a49331a5958342bba2f794483a36df46e5e12e1
takao-h <takao.hayashi@zozo.com>
2023-07-03 22:40:31 +0900

    Add and fix ExampleXXX func

--- integers/adder.go
+++ integers/adder.go
@@ -1,9 +1,13 @@
 package integers
 
+import "fmt"
+
 func Add(x, y int) int {
 	return x + y
 }
 
-func ExampleAdd(x, y int) int {
-	return x + y
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
