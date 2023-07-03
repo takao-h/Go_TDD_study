d53214bad2cb930fc4da3ba4940e39d4ba069085
takao-h <takao.hayashi@zozo.com>
2023-07-04 07:05:16 +0900

    Refactor

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,16 +1,9 @@
 package iteration
-import "fmt"
 
 func Repeat(character string, repeatCount int) string {
 	var repeat string
 	for i := 0; i < repeatCount; i++ {
-			repeat = repeat + character
+		repeat = repeat + character
 	}
-  return repeat
-}
-
-func ExampleRepeat() {
-	repeat := Repeat("a", 9)
-	fmt.Printf(repeat)
-	// Output: "aaaaaaaaa"
+	return repeat
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,7 @@
 package iteration
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -10,5 +11,16 @@ func TestRepeat(t *testing.T) {
 
 	if repeat != expected {
 		t.Errorf("expected %q but got %q", expected, repeat)
+	}
+}
+func ExampleRepeat() {
+	repeat := Repeat("a", 9)
+	fmt.Printf(repeat)
+	// Output: aaaaaaaaa
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 8)
 	}
 }
