618ae05f25cbc8f200b6606d1310d1132795508a
takao-h <takao.hayashi@zozo.com>
2023-07-05 07:46:54 +0900

    fail test

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,10 +1,11 @@
 package arrays
 
-func Sum(numbers [5]int){
-  var summed int
-	for i := 0; i < 5; i++ {
-		summed = summed +numbers[i]
-	}
+func Sum(numbers [5]int) int{
+  // var summed int
+	// for i := 0; i < 5; i++ {
+	// 	summed = summed +numbers[i]
+	// }
 
-	return summed
+	// return summed
+	return 0
 }
