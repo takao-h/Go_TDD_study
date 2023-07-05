0a43f59c0aeff3d054092fb31679038d15c05747
takao-h <takao.hayashi@zozo.com>
2023-07-06 08:40:38 +0900

    Refactor

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -4,10 +4,7 @@ import (
 	"testing"
 )
 
-type Rectangle struct {
-    Width float64
-    Height float64
-}
+
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -20,10 +17,12 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10, 10)
-	want := 100.0
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
+	want := 72.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
+
