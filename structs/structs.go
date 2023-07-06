7c4898be43dcb614ace10a9fb1fa439bf8e90aa7
takao-h <takao.hayashi@zozo.com>
2023-07-06 09:36:04 +0900

    Add table driven test and Triangle Area

--- structs/structs.go
+++ structs/structs.go
@@ -1,8 +1,35 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
-    Width float64
+	Width  float64
+	Height float64
+}
+
+type Triangle struct {
+    Base   float64
     Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Shape interface {
+    Area() float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (t Triangle) Area() float64 {
+    return 0
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -17,12 +17,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+    areaTests := []struct {
+        shape Shape
+        want  float64
+    }{
+        {Rectangle{12, 6}, 72.0},
+        {Circle{10}, 314.1592653589793},
+				{Triangle{12, 6}, 36.0},
+    }
+
+    for _, tt := range areaTests {
+        got := tt.shape.Area()
+        if got != tt.want {
+            t.Errorf("got %g want %g", got, tt.want)
+        }
+    }
+
 }
-
