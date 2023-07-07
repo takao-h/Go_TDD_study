34c1e8a21637a3ddca1659d59490e865379df848
takao-h <takao.hayashi@zozo.com>
2023-07-07 23:52:47 +0900

    pass the test

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -6,11 +6,11 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	return w.balance
 }
