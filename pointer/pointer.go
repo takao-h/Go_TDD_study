42128eb09b621bf29e4d61610c44ca3f2d0f20d8
takao-h <takao.hayashi@zozo.com>
2023-07-07 23:51:18 +0900

    check pointer of balance

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -1,10 +1,13 @@
 package pointer
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
--- pointer/poniter_test.go
+++ pointer/poniter_test.go
@@ -1,12 +1,16 @@
 package pointer
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
+	fmt.Printf("address of balance in the test is %v \n", &wallet.balance)
 	want := 10
 
 	if got != want {
