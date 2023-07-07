c6828fd386b9f02731f6880e7a8840b27368d989
takao-h <takao.hayashi@zozo.com>
2023-07-08 00:12:35 +0900

    Consolidated similar logic into a function

--- pointer/poniter_test.go
+++ pointer/poniter_test.go
@@ -18,28 +18,13 @@ func TestWallet(t *testing.T) {
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-
-		// got := wallet.Balance()
-		// fmt.Printf("address of balance in the test is %v \n", &wallet.balance)
-		// want := Bitcoin(10)
-
-		// if got != want {
-		// 	t.Errorf("got %s want %s", got, want)
-		// }
-
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 }
