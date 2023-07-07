0cf060b0282d489b1a8cc5ecd0c486321b4fdc5c
takao-h <takao.hayashi@zozo.com>
2023-07-07 23:59:27 +0900

    Add Stringer method and change test

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -3,6 +3,15 @@ package pointer
 import "fmt"
 
 type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
 	balance Bitcoin
 }
