8fd6e3616d10743985e7941dd7d5ee83fb2e3795
takao-h <takao.hayashi@zozo.com>
2023-07-08 00:11:17 +0900

    Wip refactor

--- pointer/poniter_test.go
+++ pointer/poniter_test.go
@@ -1,23 +1,33 @@
 package pointer
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-		fmt.Printf("address of balance in the test is %v \n", &wallet.balance)
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		// got := wallet.Balance()
+		// fmt.Printf("address of balance in the test is %v \n", &wallet.balance)
+		// want := Bitcoin(10)
+
+		// if got != want {
+		// 	t.Errorf("got %s want %s", got, want)
+		// }
+
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
@@ -25,7 +35,7 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 
 		got := wallet.Balance()
-	  want := Bitcoin(10)
+		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
