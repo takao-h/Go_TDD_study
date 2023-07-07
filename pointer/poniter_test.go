e049fab82f2e3447d9b36c11f867af899be4d290
takao-h <takao.hayashi@zozo.com>
2023-07-08 00:04:42 +0900

    Add new test withdraw

--- pointer/poniter_test.go
+++ pointer/poniter_test.go
@@ -6,14 +6,30 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
 
-	got := wallet.Balance()
-	fmt.Printf("address of balance in the test is %v \n", &wallet.balance)
-	want := Bitcoin(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		got := wallet.Balance()
+		fmt.Printf("address of balance in the test is %v \n", &wallet.balance)
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+	  want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
 }
