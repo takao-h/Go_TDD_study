3577bd2c7c65e6701aad3fe1a88f0478cd92c37a
takao-h <takao.hayashi@zozo.com>
2023-07-07 23:42:52 +0900

    Add struct member

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -1,12 +1,13 @@
 package pointer
 
 type Wallet struct {
+	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
-
+	w.balance += amount
 }
 
 func (w Wallet) Balance() int {
-	return 0
+	return w.balance
 }
