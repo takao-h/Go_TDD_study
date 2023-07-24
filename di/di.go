15cef2e68c485578bddd56f9d8aabfb2bac46948
takao-h <takao.hayashi@zozo.com>
2023-07-25 08:29:17 +0900

    Update mian

--- di/di.go
+++ di/di.go
@@ -1,10 +1,19 @@
 package di
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"net/http"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
-    fmt.Fprintf(writer, "Hello, %s", name)
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
