4e5d85a40d0d16b6fbf16cbcb6b5fb3e3bcd5f5a
takao-h <takao.hayashi@zozo.com>
2023-08-07 08:46:20 +0900

    Add os

--- mock/mock.go
+++ mock/mock.go
@@ -3,11 +3,12 @@ package mock
 import (
 	"bytes"
 	"fmt"
+	"os"
 )
 
-// func main () {
-// 	Countdown()
-// }
+func main () {
+	Countdown(os.Stdout)
+}
 
 func Countdown(out *bytes.Buffer) {
 	fmt.Fprint(out, 3)
