ba71f92d5f696a14cbf1afd28bf1435bf1a823c3
takao-h <takao.hayashi@zozo.com>
2023-08-07 09:02:22 +0900

    Add interface and test

--- mock/mock.go
+++ mock/mock.go
@@ -4,17 +4,41 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func main() {
-	Countdown(os.Stdout)
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
 }
 
-func Countdown(out io.Writer) {
-	const finalWord = "Go!"
-	const countdownStart = 3
-	for i := countdownStart; i > 0; i-- {
-		fmt.Fprint(out, i)
-	}
-	fmt.Fprint(out, finalWord)
+type Sleeper interface {
+    Sleep()
+}
+
+type SpySleeper struct {
+    Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+    s.Calls++
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+    for i := countdownStart; i > 0; i-- {
+        sleeper.Sleep()
+        fmt.Fprintln(out, i)
+    }
+
+    sleeper.Sleep()
+    fmt.Fprint(out, finalWord)
 }
